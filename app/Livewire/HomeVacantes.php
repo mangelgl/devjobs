eca9458cb0eb01a8adef9bd347fe033de0bad991
Miguel Ángel García <magarcial.dev@outlook.es>
2025-09-16 19:59:16 +0200

    Home

---
+++ app/Livewire/HomeVacantes.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Vacante;
+use Livewire\Component;
+
+class HomeVacantes extends Component
+{
+    public function render()
+    {
+        $vacantes = Vacante::all();
+        return view('livewire.home-vacantes', [
+            'vacantes' => $vacantes,
+        ]);
+    }
+}
